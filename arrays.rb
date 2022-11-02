a59ffbb49a0b272c0ae24c9d6c2dcd6c01176d9d
Bobby Luly <bobbyluly@gmail.com>
2022-11-02 14:57:34 -0600

    Add r arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+names = ["Bobby", "Steve", "John", "Reed"]
+ages = [33, 29, 36, 34]
+cash_on_hand = [22.5, 44.5, 87.25, 100.25]
+cool = ["Bobby" = true, "Steve" = true, "John" = false, "Reed" = false]
